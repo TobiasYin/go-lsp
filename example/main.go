75f536a682cb119e0108a4dabe6428d5af0d9738
TobiasYin <yrt7296@gmail.com>
2021-11-04 14:04:24 +0800

    add example

---
+++ example/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"strings"
+
+	"github.com/TobiasYin/go-lsp/lsp"
+	"github.com/TobiasYin/go-lsp/lsp/defines"
+)
+
+func strPtr(str string) *string {
+	return &str
+}
+
+func main() {
+	server := lsp.NewServer(&lsp.Options{CompletionProvider: &defines.CompletionOptions{
+		TriggerCharacters: &[]string{"."},
+	}})
+	server.OnHover(func(ctx context.Context, req *defines.HoverParams) (result *defines.Hover, err error) {
+		fmt.Println(req)
+		return &defines.Hover{Contents: defines.MarkupContent{Kind: defines.MarkupKindPlainText, Value: "hello world"}}, nil
+	})
+
+	server.OnCompletion(func(ctx context.Context, req *defines.CompletionParams) (result *[]defines.CompletionItem, err error) {
+		fmt.Println(req)
+		d := defines.CompletionItemKindText
+		return &[]defines.CompletionItem{defines.CompletionItem{
+			Label:               "code",
+			Kind:                &d,
+			InsertText:          strPtr("Hello"),
+		}}, nil
+	})
+
+	server.OnDocumentFormatting(func(ctx context.Context, req *defines.DocumentFormattingParams) (result *[]defines.TextEdit, err error) {
+		fmt.Println(req)
+		line, err := ReadFile(req.TextDocument.Uri)
+		if err != nil {
+			return nil, err
+		}
+		res := []defines.TextEdit{}
+		for i, v := range line {
+			r := convertParagraphs(v)
+			if v != r {
+				res = append(res, defines.TextEdit{
+					Range: defines.Range{
+						Start: defines.Position{uint(i), 0},
+						End:   defines.Position{uint(i), uint(len(v) + 1)},
+					},
+					NewText: r,
+				})
+			}
+		}
+
+		return &res, nil
+	})
+	server.Run()
+}
+func ReadFile(filename defines.DocumentUri) ([]string, error) {
+	enEscapeUrl, _ := url.QueryUnescape(string(filename))
+	data, err := ioutil.ReadFile(enEscapeUrl[6:])
+	if err != nil {
+		return nil, err
+	}
+	content := string(data)
+	line := strings.Split(content, "\n")
+	return line, nil
+}
+
+// split paragraphs into sentences, and make the sentence first char uppercase and others lowercase
+func convertParagraphs(paragraph string) string {
+	sentences := []string{}
+	for _, sentence := range strings.Split(paragraph, ".") {
+		sentence = strings.TrimSpace(sentence)
+		s := []string{}
+		w := strings.Split(sentence, " ")
+		for i, v := range w {
+			if len(v) > 0 {
+				if i == 0 {
+					s = append(s, strings.ToUpper(v[0:1])+strings.ToLower(v[1:]))
+				} else {
+					s = append(s, strings.ToLower(v))
+				}
+			}
+		}
+		if len(s) != 0 {
+			sentences = append(sentences, strings.Join(s, " ")+".")
+		}
+	}
+	return strings.Join(sentences, " ")
+}
+
